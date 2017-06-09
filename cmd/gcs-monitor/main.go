45f31d9899e6b4977c6813d22ee5ffaa2c640526
Steve Brunton <brunton@dudeman.com>
2017-06-09 00:41:40 -0400

    figuring out the handler stuff and added the semaphore package that google cloud pubsub needed

--- cmd/gcs-monitor/main.go
+++ cmd/gcs-monitor/main.go
@@ -5,29 +5,32 @@ import (
 	"os"
 	"strings"
 
-	"go.uber.org/zap"
+	"context"
+
+	"cloud.google.com/go/pubsub"
 	"github.com/nshttpd/gcs-monitor"
+	"go.uber.org/zap"
 )
 
 func main() {
 	project := flag.String("project", "", "project pubsub queues will be in")
-	queues := flag.String("queues", "", "list of queues (comma separated) to listen to")
+	topics := flag.String("topics", "", "list of topics (comma separated) to listen to")
 	//	port := flag.Int("port", 9142, "port to listen on for /metrics scraping")
 	flag.Parse()
 
-	if *project == "" || *queues == "" {
+	if *project == "" || *topics == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	qlist := strings.Split(*queues, ",")
+	tlist := strings.Split(*topics, ",")
 
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
 	logger.Info("starting up gcs-monitor",
 		zap.String("project", *project),
-		zap.Strings("queues", qlist),
+		zap.Strings("queues", tlist),
 	)
 
 	metrics := new(gcs_monitor.PromMetrics)
@@ -38,6 +41,27 @@ func main() {
 			zap.Error(err),
 		)
 		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, *project)
+	if err != nil {
+		logger.Error("error creating pubsub client",
+			zap.String("project", *project),
+		)
+	}
+
+	defer client.Close()
+
+	for _, t := range tlist {
+		h := gcs_monitor.NewHandler(logger, metrics, 5)
+		err = h.Init(client, *project, t); if err != nil {
+			logger.Error("error initializing handler",
+				zap.String("project", *project),
+				zap.String("topic", t),
+				zap.Error(err),
+			)
+		}
 	}
 
 	os.Exit(0)
--- handler.go
+++ handler.go
@@ -1,1 +1,67 @@
 package gcs_monitor
+
+import (
+	"go.uber.org/zap"
+	"cloud.google.com/go/pubsub"
+	"context"
+	"errors"
+	"time"
+)
+
+type BucketHandler struct {
+	logger  *zap.Logger
+	metrics *PromMetrics
+	workers int
+	sub     *pubsub.Subscription
+}
+
+const (
+	subName = "gcs-monitor"
+)
+
+func NewHandler(logger *zap.Logger, metrics *PromMetrics, workers int) *BucketHandler {
+	return &BucketHandler{
+		logger: logger,
+		metrics: metrics,
+		workers: workers,
+	}
+}
+
+func (h *BucketHandler) Init(client *pubsub.Client, project string, topic string) error {
+
+	ctx := context.Background()
+
+	h.logger.Info("initializing pubsub",
+		zap.String("project", project),
+		zap.String("topic", topic),
+	)
+
+	t := client.Topic(topic)
+
+	ok, err := t.Exists(ctx); if err != nil {
+		return err
+	}
+
+	if !ok {
+		return errors.New("topic doesn't exist")
+	}
+
+	h.sub = client.Subscription(subName)
+
+	ok, err = h.sub.Exists(ctx); if err != nil {
+		return err
+	}
+
+	if !ok {
+		cfg := pubsub.SubscriptionConfig{
+			Topic: t,
+			AckDeadline: 20 * time.Second,
+		}
+
+		h.sub, err = client.CreateSubscription(ctx, subName, cfg); if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
